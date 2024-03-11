f7278abeddb0221ddaaca19fce08ba98148ae88d
toptaab <top_giveheart@hotmail.com>
2024-03-11 18:08:21 +0700

    hot fix

--- src/repository/reminder.js
+++ src/repository/reminder.js
@@ -2,7 +2,7 @@ const prisma = require("../config/prisma");
 
 
 module.exports.createReminder = async (data) => await prisma.reminder.create({data})
-module.exports.deleteReminder = async (userId,eventId) => await prisma.reminder.deleteMany({where:{AND:[userId,eventId]}})
+module.exports.deleteReminder = async (userId,eventId) => await prisma.reminder.delete({where:{AND:[userId,eventId]}})
 
 
 module.exports.deleteReminderByEventId = async (where) => await prisma.reminder.deleteMany({where})
