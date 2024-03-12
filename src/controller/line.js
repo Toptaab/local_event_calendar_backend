5049a7d954e076582700d6dca97b956a0bdaaf6e
toptaab <top_giveheart@hotmail.com>
2024-03-12 11:27:37 +0700

    add log source

--- src/controller/line.js
+++ src/controller/line.js
@@ -2,6 +2,7 @@ const utils = require("../utils");
 
 module.exports.lineWebhook = utils.catchError(async(req,res,next)=>{
     console.log(req.body)
+    console.log(req.body.events[0].source)
     console.log(req.params)
 
     res.status(200).json({message: "ok"})
