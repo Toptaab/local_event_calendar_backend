7631b7179b73d48ccff31f2a9a389c7dcc55ded7
toptaab <top_giveheart@hotmail.com>
2024-03-12 11:15:58 +0700

    fix bug line

--- src/router/line-api.js
+++ src/router/line-api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const lineRoute = express.Router()
 const c = require("../controller")
 
-lineRoute.post('/', () => c.line.lineWebhook)
+lineRoute.post('/', c.line.lineWebhook)
 // lineRoute.get('/', () => console.log("linget"))
 
 module.exports = lineRoute
