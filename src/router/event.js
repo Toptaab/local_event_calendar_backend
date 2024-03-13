1e0c631a89b01d5b701050ad582081d4bed8db9c
toptaab <top_giveheart@hotmail.com>
2024-03-13 18:20:12 +0700

    add in range event api

--- src/router/event.js
+++ src/router/event.js
@@ -10,6 +10,7 @@ const eventRoute = express.Router()
 
 // ============================== done ============================//
 eventRoute.get('/',c.event.getAll)
+eventRoute.get('/inRange',c.event.getinRange)
 eventRoute.get('/:eventId',c.event.getEvent)
 eventRoute.post('/',authentication,upload.fields([{name: 'coverImage',maxCount: 1},{name: 'image'}]), c.event.createEvent)
 eventRoute.post('/scope',c.event.getAllInScope)
