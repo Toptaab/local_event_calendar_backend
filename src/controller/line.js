bd7f59f419831254510f1c9eaac213ae9d20baa5
toptaab <top_giveheart@hotmail.com>
2024-03-15 17:09:03 +0700

    edit log

--- src/controller/line.js
+++ src/controller/line.js
@@ -7,8 +7,8 @@ module.exports.lineWebhook = utils.catchError(async (req, res, next) => {
     console.log(req.body)
     console.log("==============================================================")
     console.log(req.body.events[0].source)
-    console.log(req.body.message[0])
-    console.log(req.body.message[0].text)
+    console.log(req.body.events[0].message)
+    console.log(req.body.events[0].message.text)
 
 
     res.status(200).json({ message: "ok" })
