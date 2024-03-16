de066e8b28e4f4d4b01ec16646dd8fb68b75444b
toptaab <top_giveheart@hotmail.com>
2024-03-16 16:42:54 +0700

    add feature line

--- src/controller/line.js
+++ src/controller/line.js
@@ -10,51 +10,180 @@ module.exports.lineWebhook = utils.catchError(async (req, res, next) => {
     console.log(req.body.events[0].message)
     console.log(req.body.events[0].message.text)
 
-
     switch (req.body.events[0].message.type) {
         case "text":
             switch (req.body.events[0].message.text) {
+                // ========================== highlight ================================//
                 case "highlight":
                     const highlight = await repo.event.getHighlight()
-                    const message = []
+                    console.log(highlight, "=============================================")
+                    const highlightMessage = []
                     highlight.map((value) =>
-                        message.push({
+                        highlightMessage.push({
                             thumbnailImageUrl: value.event.coverImage,
                             imageBackgroundColor: "#FFFFFF",
                             title: value.event.title,
-                            text: value.event.description.slice(0, 10) + "...",
+                            text: value.event.description.slice(0, 20) + "...",
                             defaultAction: {
                                 type: "uri",
                                 label: "View detail",
-                                uri: `https://local-event-calendar-frontend.vercel.app/event/${value.event.id}`,
+                                uri: `${process.env.BASE_URL}/event/${value.event.id}`,
                             },
                             actions: [
                                 {
                                     type: "uri",
                                     label: "visit site",
-                                    uri: `https://local-event-calendar-frontend.vercel.app/event/${value.event.id}`,
+                                    uri: `${process.env.BASE_URL}/event/${value.event.id}`,
                                 },
                             ],
                         }),
                     )
-                    const Carousel = {
-                        "type": "template",
-                        "altText": "this is a carousel template",
-                        "template": {
-                          "type": "carousel",
-                          "columns": message,
-                          "imageAspectRatio": "rectangle",
-                          "imageSize": "cover"
-                        }
-                      }
-                      const flexMessage = {
+                    console.log(highlightMessage, "=============================================")
+                    const highlightCarousel = {
+                        type: "template",
+                        altText: "Highlight Events carousel",
+                        template: {
+                            type: "carousel",
+                            columns: highlightMessage,
+                            imageAspectRatio: "rectangle",
+                            imageSize: "cover",
+                        },
+                    }
+                    const highlightFlexMessage = {
                         to: req.body.events[0].source.userId,
-                        messages: [
-                            Carousel
-                        ],
+                        messages: [highlightCarousel],
+                    }
+                    await axios.post("https://api.line.me/v2/bot/message/push", highlightFlexMessage, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+                        },
+                    })
+                    break
+                // ========================== upcoming ================================//
+                case "upcomming":
+                    const upcoming = await repo.event.getAllUpcomimng()
+                    const upcomingMessage = []
+                    upcoming.map((value) =>
+                        upcomingMessage.push({
+                            thumbnailImageUrl: value.coverImage,
+                            imageBackgroundColor: "#FFFFFF",
+                            title: value.title,
+                            text: value.description.slice(0, 20) + "...",
+                            defaultAction: {
+                                type: "uri",
+                                label: "View detail",
+                                uri: `${process.env.BASE_URL}/event/${value.id}`,
+                            },
+                            actions: [
+                                {
+                                    type: "uri",
+                                    label: "visit site",
+                                    uri: `${process.env.BASE_URL}/event/${value.id}`,
+                                },
+                            ],
+                        }),
+                    )
+                    const upcomingCarousel = {
+                        type: "template",
+                        altText: "upcoming events carousel",
+                        template: {
+                            type: "carousel",
+                            columns: upcomingMessage,
+                            imageAspectRatio: "rectangle",
+                            imageSize: "cover",
+                        },
+                    }
+                    const upcomingflexMessage = {
+                        to: req.body.events[0].source.userId,
+                        messages: [upcomingCarousel],
+                    }
+                    await axios.post("https://api.line.me/v2/bot/message/push", upcomingflexMessage, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+                        },
+                    })
+                    break
+                // ========================== myevent ================================//
+                case "myevent":
+                    const myEvent = await repo.user.getUser({ lineToken: req.body.events[0].source.userId })
+
+                    // ===================Guard not register or binding line user======================//
+                    if (!myEvent) {
+                        const inviteMessage = {
+                            type: "template",
+                            altText: "Register invite Card",
+                            template: {
+                                type: "buttons",
+                                thumbnailImageUrl: "https://th.bing.com/th/id/OIP.UXGikObFpH0-hFMv4PqQ5AHaE8?rs=1&pid=ImgDetMain",
+                                imageAspectRatio: "rectangle",
+                                imageSize: "cover",
+                                imageBackgroundColor: "#FFFFFF",
+                                title: "Happening Thailand",
+                                text: "Please Register and binding line for set your reminder",
+                                defaultAction: {
+                                    type: "uri",
+                                    label: "View detail",
+                                    uri: process.env.BASE_URL,
+                                },
+                                actions: [
+                                    {
+                                        type: "uri",
+                                        label: "Register here",
+                                        uri: `${process.env.BASE_URL}/register`,
+                                    },
+                                ],
+                            },
+                        }
+                        const registrationInviteMessage = {
+                            to: req.body.events[0].source.userId,
+                            messages: [inviteMessage],
+                        }
+                        await axios.post("https://api.line.me/v2/bot/message/push", registrationInviteMessage, {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+                            },
+                        })
                     }
 
-                    await axios.post("https://api.line.me/v2/bot/message/push", flexMessage, {
+                    const myEventMessage = []
+                    myEvent.OrganizerInformation.Event.map((value) =>
+                        myEventMessage.push({
+                            thumbnailImageUrl: value.coverImage,
+                            imageBackgroundColor: "#FFFFFF",
+                            title: value.title,
+                            text: value.description.slice(0, 20) + "...",
+                            defaultAction: {
+                                type: "uri",
+                                label: "View detail",
+                                uri: `${process.env.BASE_URL}/event/${value.id}`,
+                            },
+                            actions: [
+                                {
+                                    type: "uri",
+                                    label: "visit site",
+                                    uri: `${process.env.BASE_URL}/event/${value.id}`,
+                                },
+                            ],
+                        }),
+                    )
+                    const myEventMessageCarousel = {
+                        type: "template",
+                        altText: "this is a carousel template",
+                        template: {
+                            type: "carousel",
+                            columns: myEventMessage,
+                            imageAspectRatio: "rectangle",
+                            imageSize: "cover",
+                        },
+                    }
+                    const myEventflexMessage = {
+                        to: req.body.events[0].source.userId,
+                        messages: [myEventMessageCarousel],
+                    }
+                    await axios.post("https://api.line.me/v2/bot/message/push", myEventflexMessage, {
                         headers: {
                             "Content-Type": "application/json",
                             Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
@@ -62,14 +191,90 @@ module.exports.lineWebhook = utils.catchError(async (req, res, next) => {
                     })
 
                     break
-                case "my event":
-                    console.log("my event===========================")
-                    break
-                case "upcomming":
-                    console.log("upcomming===========================")
-                    break
-                case "my reminder":
-                    console.log("upcomming===========================")
+                // ========================== Reminder ================================//
+                case "myreminder":
+                    const myReminder = await repo.user.getUser({ lineToken: req.body.events[0].source.userId })
+
+                    // ===================Guard not register or binding line user======================//
+                    if (!myReminder) {
+                        const inviteMessage = {
+                            type: "template",
+                            altText: "Register invite Card",
+                            template: {
+                                type: "buttons",
+                                thumbnailImageUrl: "https://th.bing.com/th/id/OIP.UXGikObFpH0-hFMv4PqQ5AHaE8?rs=1&pid=ImgDetMain",
+                                imageAspectRatio: "rectangle",
+                                imageSize: "cover",
+                                imageBackgroundColor: "#FFFFFF",
+                                title: "Happening Thailand",
+                                text: "Please Register and binding line for set your reminder",
+                                defaultAction: {
+                                    type: "uri",
+                                    label: "View detail",
+                                    uri: process.env.BASE_URL,
+                                },
+                                actions: [
+                                    {
+                                        type: "uri",
+                                        label: "Register here",
+                                        uri: `${process.env.BASE_URL}/register`,
+                                    },
+                                ],
+                            },
+                        }
+                        const registrationInviteMessage = {
+                            to: req.body.events[0].source.userId,
+                            messages: [inviteMessage],
+                        }
+                        await axios.post("https://api.line.me/v2/bot/message/push", registrationInviteMessage, {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+                            },
+                        })
+                    }
+
+                    const myReminderMessage = []
+                    myReminder.Reminder.map((value) =>
+                        myReminderMessage.push({
+                            thumbnailImageUrl: value.event.coverImage,
+                            imageBackgroundColor: "#FFFFFF",
+                            title: value.event.title,
+                            text: value.event.description.slice(0, 20) + "...",
+                            defaultAction: {
+                                type: "uri",
+                                label: "View detail",
+                                uri: `${process.env.BASE_URL}/event/${value.event.id}`,
+                            },
+                            actions: [
+                                {
+                                    type: "uri",
+                                    label: "visit site",
+                                    uri: `${process.env.BASE_URL}/event/${value.event.id}`,
+                                },
+                            ],
+                        }),
+                    )
+                    const myReminderMessageCarousel = {
+                        type: "template",
+                        altText: "this is a carousel template",
+                        template: {
+                            type: "carousel",
+                            columns: myReminderMessage,
+                            imageAspectRatio: "rectangle",
+                            imageSize: "cover",
+                        },
+                    }
+                    const myReminderflexMessage = {
+                        to: req.body.events[0].source.userId,
+                        messages: [myReminderMessageCarousel],
+                    }
+                    await axios.post("https://api.line.me/v2/bot/message/push", myReminderflexMessage, {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+                        },
+                    })
                     break
                 default:
                     break
@@ -122,13 +327,13 @@ module.exports.pushContent = utils.catchError(async (req, res, next) => {
                             defaultAction: {
                                 type: "uri",
                                 label: "View detail",
-                                uri: `http://localhost:5173/event/${reminder[i].event.id}`,
+                                uri: `${process.env.BASE_URL}/event/${reminder[i].event.id}`,
                             },
                             actions: [
                                 {
                                     type: "uri",
                                     label: "visit Site",
-                                    uri: `http://localhost:5173/event/${reminder[i].event.id}`,
+                                    uri: `${process.env.BASE_URL}/event/${reminder[i].event.id}`,
                                 },
                             ],
                         },
